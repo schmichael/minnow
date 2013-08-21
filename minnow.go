675742fb6406219ad898be3dc35595c8546972f5
Kyle Jones <kyle.jones@urbanairship.com>
2013-08-21 07:38:18 -0700

    Parameterize number of chaffers; formatting

--- minnow.go
+++ minnow.go
@@ -8,7 +8,6 @@ import (
 	"hash"
 	"io"
 	"log"
-	"sync"
 
 	cryptoRand "crypto/rand"
 	mathRand "math/rand"
@@ -18,8 +17,8 @@ var ErrAlreadyClosed = errors.New("Writer has already been closed")
 var ErrMaxLengthExceeded = errors.New("Maximum message length (1<<64 - 1) exceeded")
 
 type FakePacketResult struct {
-    Hasher hash.Hash
-    Message []byte
+	Hasher  hash.Hash
+	Message []byte
 }
 
 type Packet struct {
@@ -38,7 +37,7 @@ type MessageWriteCloser struct {
 	hash        hash.Hash
 	message     []byte
 	destination io.WriteCloser
-	wg          *sync.WaitGroup
+	numchaff    int
 }
 
 func NewMessageWriteCloser(secret []byte, w io.WriteCloser) *MessageWriteCloser {
@@ -47,7 +46,7 @@ func NewMessageWriteCloser(secret []byte, w io.WriteCloser) *MessageWriteCloser 
 		hash:        hmac.New(sha512.New, secret),
 		message:     make([]byte, 0),
 		destination: w,
-		wg:          new(sync.WaitGroup),
+		numchaff:    1000, // FIXME: Randomize?
 	}
 }
 
@@ -61,31 +60,31 @@ func (mw *MessageWriteCloser) Write(m []byte) (int, error) {
 // Close is when we actually get the end of the message and can write it
 func (mw *MessageWriteCloser) Close() error {
 	if !mw.closed {
-        defer mw.destination.Close()
+		defer mw.destination.Close()
 		log.Printf("Writing message:\n%s", string(mw.message))
 		mw.closed = true
-        packetchans := make([]chan *Packet, 11)
-        packetchans[0] = sequentialPacketChannel(mw.message, mw.hash)
+		numchans := mw.numchaff + 1
+		packetchans := make([]chan *Packet, numchans)
+		packetchans[0] = sequentialPacketChannel(mw.message, mw.hash)
 		// Write chaff asynchronously
-        // FIXME: Random number of chaffers
-        for i := 1; i < 11; i++ {
-            // FIXME: Length should be randomized
-            faker, err := fakeMessage(len(mw.message))
-            if err != nil {
-                return err
-            }
-            packetchans[i] = sequentialPacketChannel(faker.Message, faker.Hasher)
-        }
+		for i := 1; i < numchans; i++ {
+			// FIXME: Length should be randomized
+			faker, err := fakeMessage(len(mw.message))
+			if err != nil {
+				return err
+			}
+			packetchans[i] = sequentialPacketChannel(faker.Message, faker.Hasher)
+		}
 
-        outpackets := randomPacketRouter(packetchans)
+		outpackets := randomPacketRouter(packetchans)
 
-        // FIXME: Randomize the packets/order
-        for p := range outpackets {
-            err := mw.writePacket(p)
-            if err != nil {
-                return err
-            }
-        }
+		// FIXME: Randomize the packets/order
+		for p := range outpackets {
+			err := mw.writePacket(p)
+			if err != nil {
+				return err
+			}
+		}
 
 		return mw.destination.Close()
 	}
@@ -93,7 +92,7 @@ func (mw *MessageWriteCloser) Close() error {
 }
 
 func (mw *MessageWriteCloser) writePacket(packet *Packet) error {
-    log.Printf("Writing packet: #%d - %+v\n", packet.Header.SequenceN, packet.Payload)
+	log.Printf("Writing packet: #%d - %+v\n", packet.Header.SequenceN, packet.Payload)
 	err := binary.Write(mw.destination, binary.BigEndian, packet.Header)
 	if err != nil {
 		return err
@@ -169,77 +168,77 @@ func (r *MessageReader) matches(data []byte, provided [64]byte) bool {
 // Send len(data) # of sequential packets, hashed with the provided hash.Hash
 // down the provided channel
 func sequentialPacketChannel(data []byte, hasher hash.Hash) chan *Packet {
-    // FIXME: Packet size is hard-coded at 1
-    outchan := make(chan *Packet)
-    go func() {
-        for i, _ := range data {
-            var mbuf [64]byte
+	// FIXME: Packet size is hard-coded at 1
+	outchan := make(chan *Packet)
+	go func() {
+		for i, _ := range data {
+			var mbuf [64]byte
 
-            hasher.Write(data[i:i+1])
-            m := hasher.Sum(nil)
-            for i, _ := range mbuf {
-                mbuf[i] = m[i]
-            }
-            hasher.Reset()
+			hasher.Write(data[i : i+1])
+			m := hasher.Sum(nil)
+			for i, _ := range mbuf {
+				mbuf[i] = m[i]
+			}
+			hasher.Reset()
 
-            h := PacketHeader{
-                SequenceN: uint64(i),
-                Mac:       mbuf,
-                Size:      uint64(1),
-            }
-            outchan<- &Packet{h, data[i:i+1]}
-        }
+			h := PacketHeader{
+				SequenceN: uint64(i),
+				Mac:       mbuf,
+				Size:      uint64(1),
+			}
+			outchan <- &Packet{h, data[i : i+1]}
+		}
 
-        outchan<- nil
-    }()
+		outchan <- nil
+	}()
 
-    return outchan
+	return outchan
 }
 
 // Create a mock hasher and buffer
 func fakeMessage(packetSize int) (*FakePacketResult, error) {
-    var err error
+	var err error
 
-    payloadbuf := make([]byte, packetSize, packetSize)
+	payloadbuf := make([]byte, packetSize, packetSize)
 
-    sz := uint64(0)
-    for sz == 0 {
-        sz = uint64(mathRand.Int63()) / 1000000000000000
-    }
-    hashseed := make([]byte, sz, sz)
+	sz := uint64(0)
+	for sz == 0 {
+		sz = uint64(mathRand.Int63()) / 1000000000000000
+	}
+	hashseed := make([]byte, sz, sz)
 
-    _, err = cryptoRand.Read(hashseed)
-    if err != nil {
-        return nil, err
-    }
+	_, err = cryptoRand.Read(hashseed)
+	if err != nil {
+		return nil, err
+	}
 
-    h := hmac.New(sha512.New, hashseed)
+	h := hmac.New(sha512.New, hashseed)
 
-    _, err = cryptoRand.Read(payloadbuf)
-    if err != nil {
-        return nil, err
-    }
+	_, err = cryptoRand.Read(payloadbuf)
+	if err != nil {
+		return nil, err
+	}
 
-    return &FakePacketResult{h, payloadbuf}, nil
+	return &FakePacketResult{h, payloadbuf}, nil
 }
 
 // Radomly select packets from the provided channels until they
 // are all depleted (received a nil)
 func randomPacketRouter(inchans []chan *Packet) chan *Packet {
-    outchan := make(chan *Packet)
-    go func() {
-        for len(inchans) > 0 {
-            ndx := mathRand.Intn(len(inchans))
-            val := <-inchans[ndx]
-            // This packet provider is done
-            if val == nil {
-                inchans = append(inchans[:ndx], inchans[ndx+1:]...)
-            } else {
-                outchan<- val
-            }
-        }
-        close(outchan)
-    }()
+	outchan := make(chan *Packet)
+	go func() {
+		for len(inchans) > 0 {
+			ndx := mathRand.Intn(len(inchans))
+			val := <-inchans[ndx]
+			// This packet provider is done
+			if val == nil {
+				inchans = append(inchans[:ndx], inchans[ndx+1:]...)
+			} else {
+				outchan <- val
+			}
+		}
+		close(outchan)
+	}()
 
-    return outchan
+	return outchan
 }
